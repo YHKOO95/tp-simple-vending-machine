787d552dcc076801a09d0c7d91b016be784c2ab1
YHKOO95 <51323315+YHKOO95@users.noreply.github.com>
2021-01-11 19:46:26 +0900

    FIX] layout 기반 추가

--- app/src/main/java/com/tp2021/tp_simple_vending_machine/base/BaseActivity.kt
+++ app/src/main/java/com/tp2021/tp_simple_vending_machine/base/BaseActivity.kt
@@ -1,6 +1,7 @@
 package com.tp2021.tp_simple_vending_machine.base
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
--- app/src/main/java/com/tp2021/tp_simple_vending_machine/view/MainActivity.kt
+++ app/src/main/java/com/tp2021/tp_simple_vending_machine/view/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.tp2021.tp_simple_vending_machine.view
 
-import android.annotation.SuppressLint
-import android.widget.Toast
 import com.tp2021.tp_simple_vending_machine.R
 import com.tp2021.tp_simple_vending_machine.base.BaseActivity
 
@@ -10,10 +8,8 @@ class MainActivity : BaseActivity() {
     override val layoutId: Int
         get() = R.layout.activity_main
 
-    @SuppressLint("ShowToast")
     override fun onActivityCreate() {
 
-        Toast.makeText(this, "test", Toast.LENGTH_SHORT)
 
 
 

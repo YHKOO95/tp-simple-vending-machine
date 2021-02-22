c337a41e181dd64f71226f4a1cf9f69b4bd002de
YHKOO95 <>
2021-02-23 01:49:23 +0900

    ADD] Fragment View 생성, kotlin extension 추가

---
+++ app/src/main/java/com/tp2021/tp_simple_vending_machine/view/home/HomeFragment.kt
@@ -0,0 +1,39 @@
+package com.tp2021.tp_simple_vending_machine.view.home
+
+import com.tp2021.tp_simple_vending_machine.R
+import com.tp2021.tp_simple_vending_machine.base.BaseFragment
+import com.tp2021.tp_simple_vending_machine.databinding.FragmentHomeBinding
+import com.tp2021.tp_simple_vending_machine.viewModel.MainViewModel
+import kotlinx.android.synthetic.main.fragment_home.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class HomeFragment : BaseFragment<FragmentHomeBinding, MainViewModel>(){
+
+    override val layoutId: Int
+        get() = R.layout.fragment_home
+
+    override val viewModel: MainViewModel by viewModel()
+
+    override fun initStartView() {
+
+        val name = "Administrator"
+
+        introTextView.text = "Hello, $name !!"
+
+
+
+
+    }
+
+    override fun initDataBinding() {
+
+
+
+    }
+
+    override fun initAfterBinding() {
+
+
+
+    }
+}
